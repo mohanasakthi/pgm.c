aa6a33d2360466e7c3d60a5d06c9944cacd2d51d
mohanasakthi <46813546+mohanasakthi@users.noreply.github.com>
2019-02-20 16:06:12 +0530

    Create b9.c

---
+++ b9.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int m,k,i,j,sum=0;
+printf(" values:");
+scanf("%d\t%d",&m,&k);
+for(i=1;i<=m;i++)
+{
+printf("%d\m",m);
+}
+for(j=1;j<=k;j++)
+{
+sum = sum+j;
+}
+printf(" sum is %d",sum);
+return 0;
+}
