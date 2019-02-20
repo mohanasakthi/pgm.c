670ef77fdbf8a03f8b07b9e0505e8028d084de09
mohanasakthi <46813546+mohanasakthi@users.noreply.github.com>
2019-02-20 16:02:39 +0530

    Create b8.c

---
+++ b8.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,s,sum=0;
+printf("enter the number");
+scanf("%d",&s);
+for(i=0;i<=s;++i)
+{
+sum=sum+i;}
+printf("the sum i %d",sum);
+}
