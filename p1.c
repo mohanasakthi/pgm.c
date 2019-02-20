cbe775e2d67420fa0c8190a95183f4b7bcca060b
mohanasakthi <46813546+mohanasakthi@users.noreply.github.com>
2019-02-20 15:33:38 +0530

    Update p1.c

--- p1.c
+++ p1.c
@@ -6,15 +6,15 @@ int n;
 scanf("%d",&n);
 if(n>0)
 {
-printf("The no is positive");
+printf(" p");
 }
 else if(n<0)
 {
-printf("The no is negative");
+printf("n");
 }
 else
 {
-printf("The no is equal to 0");
+printf(" 0");
 }
 return 0;
 }
