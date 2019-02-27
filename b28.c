e52527526dd7a482eb7085879928279afb9f5cef
mohanasakthi <46813546+mohanasakthi@users.noreply.github.com>
2019-02-27 12:19:24 +0530

    Create b28.c

---
+++ b28.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int n,temp,i,j,a[20];
+printf("enter the array of numbers");
+scanf("%d",&n);
+printf(" the numbers");
+for(i=0;i<=n;i++)
+{
+scanf("%d",&a[i]);
+}
+
+for(i=0;i<=n;i++)
+{
+    printf("%d%d",a[i],i);
+}
+}
