06c10f704f23f7e7661fb66a211eea40530e6a19
mohanasakthi <46813546+mohanasakthi@users.noreply.github.com>
2019-02-27 12:36:58 +0530

    Create b39.c

---
+++ b39.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+	int a[5],i,b;
+	printf(" array value");
+	for(i=0;i<=5;i++)
+	{
+		scanf("%d",a[i]);
+	}
+	b=a[0];
+	for(i=0;i<=5;i++)
+	{
+		if(b>a[i])
+		{
+			b=b;
+		}
+		else
+		{
+			b=a[i];
+		}
+	}
+	printf("max%d",b);
+}
