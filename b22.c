e1e018679832d11fcc95689094271064de6c8dcb
mohanasakthi <46813546+mohanasakthi@users.noreply.github.com>
2019-02-27 12:08:01 +0530

    Create b22.c

---
+++ b22.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+int a[50],i,n,max;
+printf("enter the range");
+scanf("%d",&n);
+printf(" array values");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+max=a[0];
+for(i=0;i<n;i++)
+{
+if(a[i]>max)
+{
+max=a[i];
+}
+}
+printf("%d",max);
+}
