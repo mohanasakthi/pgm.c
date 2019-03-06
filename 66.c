8e23fa1bfbbe713d1e9006e5e71754a5471d4094
mohanasakthi <46813546+mohanasakthi@users.noreply.github.com>
2019-03-06 19:36:06 +0530

    Create 66.c

---
+++ 66.c
@@ -0,0 +1,20 @@
+#include<stdio.h>  
+int main(){    
+int n,i,m=0,flag=0;    
+   
+scanf("%d",&n);    
+m=n/2;    
+for(i=2;i<=m;i++)    
+{    
+if(n%i==0)    
+{    
+printf("yes");    
+flag=1;    
+break;    
+}    
+}    
+
+if(flag==0)    
+printf("no");     
+return 0;  
+ }    
