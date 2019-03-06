275f26a78f0c6adafc35e08bde2664385e0b8765
mohanasakthi <46813546+mohanasakthi@users.noreply.github.com>
2019-03-06 17:32:31 +0530

    Create 53.c

---
+++ 53.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int n,i=0,x=0,sum=0;
+	printf("enter the num ");
+	scanf("%d",&n);
+	while(n>0)
+	{
+		x=n%10;
+		n=n/10;
+		sum=sum+x;
+	}
+
+
+	printf("%d ",sum);
+}
