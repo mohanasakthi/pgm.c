e49757f039d1f0581ea3a8fc38e94f7100019e9d
mohanasakthi <46813546+mohanasakthi@users.noreply.github.com>
2019-03-08 14:41:54 +0530

    Create 73.c

---
+++ 73.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	int m,n,o;
+	scanf("%d %d %d",&m,&n,&o);
+	
+	if(m>n)
+	{
+		if(m<o)
+		printf("yes");
+	}
+  
+	else
+	{
+		printf("no");
+	}
+	return 0;
+}
