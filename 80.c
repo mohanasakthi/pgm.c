d99e1d70956ba5ae9d8ceaa886c6f47e14cbccf1
mohanasakthi <46813546+mohanasakthi@users.noreply.github.com>
2019-03-12 11:17:18 +0530

    Create 80.c

---
+++ 80.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    while(n>0)
+    {
+    	x=n%10;
+    	if(x%2!=0)
+    	{
+    		printf("%d ",x);	
+		}
+	  	n=n/10;
+	}
+    
+}
